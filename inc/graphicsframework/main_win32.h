0cb8d3ad4106c5acbc595265d0d70b1117211734
bransay <48835025+bransay@users.noreply.github.com>
2019-04-05 16:17:41 +0100

    Added basic lifecycle and application code

--- inc/graphicsframework/main_win32.h
+++ inc/graphicsframework/main_win32.h
@@ -2,12 +2,26 @@
 #define GF_MAIN_WIN32_H_
 
 #include "graphicsframework/Config.h"
+#include "graphicsframework/Application.h"
 
 #ifdef WIN32
 #include <Windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    {
+        using namespace GraphicsFramework;
+        if (auto application = Application::GetPrimary())
+        {
+            application->Start();
+            application->End();
+        }
+        else
+        {
+            // TODO: error handling?
+        }
+    }
+
 	return 0;
 }
 #endif
--- src/graphicsframework/Application.cpp
+++ src/graphicsframework/Application.cpp
@@ -2,11 +2,24 @@
 
 namespace GraphicsFramework
 {
-	Application::Application()
-	{
-	}
+    Application* Application::ms_Primary = nullptr;
 
-	Application::~Application()
-	{
-	}
+    Application::Application(ILifeCycle& lifeCycle)
+        : m_LifeCycle(&lifeCycle)
+    {
+    }
+
+    Application::~Application()
+    {
+    }
+
+    void Application::Start()
+    {
+        m_LifeCycle->OnStart();
+    }
+
+    void Application::End()
+    {
+        m_LifeCycle->OnEnd();
+    }
 }
